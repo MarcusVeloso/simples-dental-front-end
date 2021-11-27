d76500fe18f4fc97959577fbfacf571bf4f0befb
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-11-27 11:38:39 -0300

    criacao da arquitetura do projeto

---
+++ src/app/albums/albums-details/albums-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-albums-details',
+  templateUrl: './albums-details.component.html',
+  styleUrls: ['./albums-details.component.css']
+})
+export class AlbumsDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './autenticacao/login/login.component';
+import { RegistrarComponent } from './autenticacao/registrar/registrar.component';
+import { AlbumsListComponent } from './albums/albums-list/albums-list.component';
+import { AlbumsDetailsComponent } from './albums/albums-details/albums-details.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrarComponent,
+    AlbumsListComponent,
+    AlbumsDetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
